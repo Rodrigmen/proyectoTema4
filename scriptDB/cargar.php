1a482a9ab0cd41ccaf6b1c1bf04e35cc13522fee
Rodrigmen <roblesminambresrodrigo@gmail.com>
2020-11-27 09:26:01 +0100

    Update files

--- scriptDB/cargar.php
+++ scriptDB/cargar.php
@@ -22,7 +22,7 @@ try {
 
     $miDB->exec($sql);
 
-    echo "<h3> <span style='color: green;'>" . "Tabla creada correctamente</span></h3>"; //Si no se ha producido ningún error nos mostrará "Conexión establecida con éxito"
+    echo "<h3> <span style='color: green;'>" . "Tabla cargada correctamente</span></h3>"; //Si no se ha producido ningún error nos mostrará "Conexión establecida con éxito"
 } catch (PDOException $excepcion) {//Código que se ejecutará si se produce alguna excepción
     $errorExcepcion = $excepcion->getCode(); //Almacenamos el código del error de la excepción en la variable $errorExcepcion
     $mensajeExcepcion = $excepcion->getMessage(); //Almacenamos el mensaje de la excepción en la variable $mensajeExcepcion
